1967e54b088ab238fad435128918fc1ea8d8f791
subhash-kumar <designdot.dev4@gmail.com>
2023-04-05 23:14:47 +0530

    This is a new commit for what I originally planned to be amended

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+import { Footer, Blog, Possibility, Feature, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components/Index';
+import './App.css';
+
+const  App = () => {
+  return (
+    <div className="App">
+      <div className='gradient__bg'>
+      <Navbar />
+      <Header />
+      </div>
+      <Brand />
+    <WhatGPT3 />
+    <Feature />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
